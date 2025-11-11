014e986e91a35eb07f557dea015511aece567f5c
RyanDynamix <ryannguyen070823@gmail.com>
2025-11-11 16:25:03 +0700

    Hưng

---
+++ src/java/Email/Constants.java
@@ -0,0 +1,18 @@
+package Email;
+
+
+public class Constants {
+
+	public static String GOOGLE_CLIENT_ID = "804818262350-40vfko0abovcknrorrak2bdnhtl7nj07.apps.googleusercontent.com";
+
+	public static String GOOGLE_CLIENT_SECRET = "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";
+
+	public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/THI247/LoginGoogleHandler";
+
+	public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+
+	public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+
+	public static String GOOGLE_GRANT_TYPE = "authorization_code";
+
+}
